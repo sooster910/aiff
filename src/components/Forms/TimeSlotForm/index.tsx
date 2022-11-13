ccfc8aec6e8543a0038ca90ac1b9fd6cec363bc7
Hyunsu <hs9880@gmail.com>
2022-11-13 14:09:43 +0900

    [ADD] banner image

--- src/components/Forms/TimeSlotForm/index.tsx
+++ src/components/Forms/TimeSlotForm/index.tsx
@@ -1,20 +1,20 @@
-import * as React from 'react'
-import { Grid, Radio, Spinner, Text } from '@geist-ui/core'
-import { TimeSlotDTO } from '@app/types/timeslot'
+import * as React from "react";
+import { Grid, Radio, Spinner, Text } from "@geist-ui/core";
+import { TimeSlotDTO } from "@app/types/timeslot";
 // import { graphql } from 'relay-runtime'
 // import { useLazyLoadQuery } from 'react-relay'
-import { DateTime } from 'luxon'
-import { aiffAPI } from '@app/utils/aiffAPI'
-import useSWR from 'swr'
+import { DateTime } from "luxon";
+import { aiffAPI } from "@app/utils/aiffAPI";
+import useSWR from "swr";
 // import useSWR from 'swr'
 // import { aiffAPI } from '@app/utils/aiffAPI'
 type TimeSlotFormProps = {
-  storeId?: string
-  classId?: string
-  timeSlots?: TimeSlotDTO[]
-  handleSelectTimeSlot: (timeslotValue) => void
-  date?: Date // TODO: DATE? OR STRING?
-}
+  storeId?: string;
+  classId?: string;
+  timeSlots?: TimeSlotDTO[];
+  handleSelectTimeSlot: (timeslotValue) => void;
+  date?: Date; // TODO: DATE? OR STRING?
+};
 
 export const TimeSlotForm: React.FunctionComponent<TimeSlotFormProps> = ({
   storeId,
@@ -22,8 +22,8 @@ export const TimeSlotForm: React.FunctionComponent<TimeSlotFormProps> = ({
   date,
   handleSelectTimeSlot,
 }) => {
-  console.log('timeslotForm')
-  const [selectedTimeSlot, setState] = React.useState<string | null>(null)
+  console.log("timeslotForm");
+  const [selectedTimeSlot, setState] = React.useState<string | null>(null);
   const {
     data: timeSlots,
     error,
@@ -33,12 +33,12 @@ export const TimeSlotForm: React.FunctionComponent<TimeSlotFormProps> = ({
       date
     ).toISODate()}&storeId=${storeId}`,
     {
-      fetcher: async key => {
-        const res = await aiffAPI.get(key)
-        return { timeSlots: res.data }
+      fetcher: async (key) => {
+        const res = await aiffAPI.get(key);
+        return { timeSlots: res.data };
       },
     }
-  )
+  );
 
   // const data = useLazyLoadQuery(
   //   graphql`
@@ -82,29 +82,29 @@ export const TimeSlotForm: React.FunctionComponent<TimeSlotFormProps> = ({
   // )
   // const timeSlots = data['availableTimeSlotsByClass']
 
-  if (error) return <h1> 페이지를 불러 오는데 실패 했습니다. </h1>
-  if (!timeSlots && isValidating && !error) return <Spinner />
+  if (error) return <h1> 페이지를 불러 오는데 실패 했습니다. </h1>;
+  if (!timeSlots && isValidating && !error) return <Spinner />;
   if (timeSlots) {
     return (
       <>
         <Grid>
           <Text h5 mt={1}>
-            {' '}
-            클래스 시간{' '}
+            {" "}
+            클래스 시간{" "}
           </Text>
         </Grid>
-        <Grid.Container style={{ marginTop: '1rem', overflow: 'scroll' }}>
-          <div style={{ margin: '1rem' }}>
+        <Grid.Container style={{ marginTop: "1rem", overflow: "scroll" }}>
+          <div style={{ margin: "1rem" }}>
             <Radio.Group
               useRow
               value={selectedTimeSlot}
               onChange={(timeSlotId: string) => {
                 const selectedTimeslot = timeSlots?.timeSlots?.find(
-                  v => Number(v.id) === Number(timeSlotId)
-                )
-                setState(timeSlotId)
+                  (v) => Number(v.id) === Number(timeSlotId)
+                );
+                setState(timeSlotId);
                 // handleSelectTimeSlot(DateTime.fromISO(value).toFormat('HH:mm'))
-                handleSelectTimeSlot(selectedTimeslot)
+                handleSelectTimeSlot(selectedTimeslot);
               }}
             >
               {timeSlots?.timeSlots?.length > 0 ? (
@@ -115,36 +115,41 @@ export const TimeSlotForm: React.FunctionComponent<TimeSlotFormProps> = ({
                         timeslot?.id
                       }`}
                       style={{
-                        border: '1px solid black',
-                        borderRadius: '10px',
-                        padding: '7px 13px',
-                        marginRight: '1rem',
+                        border: "1px solid black",
+                        borderRadius: "10px",
+                        padding: "7px 13px",
+                        marginRight: "1rem",
                       }}
                     >
+                      <button id={timeslot?.id} color="primary">
+                        {`${DateTime.fromISO(timeslot.startDateTime).toFormat(
+                          "HH:mm"
+                        )}`}
+                      </button>
                       <Radio
                         id={timeslot?.id}
                         value={timeslot.id}
                         color="primary"
                         size={2}
                         style={{
-                          marginLeft: '2px',
-                          fontSize: '2rem',
-                          border: '1px solid black',
-                          marginRight: '7px',
+                          marginLeft: "2px",
+                          fontSize: "2rem",
+                          border: "1px solid black",
+                          marginRight: "7px",
                         }}
-                        name={'store'}
-                        type={'secondary'}
+                        name={"store"}
+                        type={"secondary"}
                         disabled={
                           timeslot?.currentBookingCount ===
                           timeslot?.maximumBookingCount
                         }
                       >
                         {`${DateTime.fromISO(timeslot.startDateTime).toFormat(
-                          'HH:mm'
+                          "HH:mm"
                         )}`}
                       </Radio>
                     </div>
-                  )
+                  );
                 })
               ) : (
                 <p>해당하는 클래스가 없습니다.</p>
@@ -153,8 +158,8 @@ export const TimeSlotForm: React.FunctionComponent<TimeSlotFormProps> = ({
           </div>
         </Grid.Container>
       </>
-    )
+    );
   } else {
-    return null
+    return null;
   }
-}
+};
