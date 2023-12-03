611b6e3bcbca6a67178c54134bb0392745fe891b
Hyunsu <hs9880@gmail.com>
2023-12-03 22:16:11 +0900

    [ADD] sentry

--- next.config.js
+++ next.config.js
@@ -8,6 +8,57 @@ const nextConfig = {
   images: {
     domains: ["aiff.co.kr", "aiff-image-upload.s3.us-east-1.amazonaws.com"],
   },
+  compiler: {
+    relay: {
+      // This should match relay.config.js
+      src: ".",
+      schema: "./schema.graphql",
+      exclude: [
+        "**/node_modules/**",
+        "**/__mocks__/**",
+        "**/__generated__/**",
+        "**/.next/**",
+      ],
+      artifactDirectory: "__generated__",
+      language: "typescript",
+    },
+  },
 };
 
 module.exports = nextConfig;
+
+// Injected content via Sentry wizard below
+
+const { withSentryConfig } = require("@sentry/nextjs");
+
+module.exports = withSentryConfig(
+  module.exports,
+  {
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options
+
+    // Suppresses source map uploading logs during build
+    silent: true,
+    org: "aiff",
+    project: "javascript-nextjs",
+  },
+  {
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
+
+    // Transpiles SDK to be compatible with IE11 (increases bundle size)
+    transpileClientSDK: true,
+
+    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+    tunnelRoute: "/monitoring",
+
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+  }
+);
--- relay.config.js
+++ relay.config.js
@@ -1,12 +1,13 @@
 module.exports = {
-  src: '.',
-  schema: './schema.graphql',
+  src: ".",
+  schema: "./schema.graphql",
   exclude: [
-    '**/node_modules/**',
-    '**/__mocks__/**',
-    '**/__generated__/**',
-    '**/.next/**',
+    "**/node_modules/**",
+    "**/__mocks__/**",
+    "**/__generated__/**",
+    "**/.next/**",
   ],
-  artifactDirectory: '__generated__',
-  language: 'typescript',
-}
+  artifactDirectory: "__generated__",
+  language: "typescript",
+  eagerEsModules: false,
+};
