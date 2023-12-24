4b551bcf89dfa3ea47d2d0ee2c9fc73c0c429f8c
Hyunsu <hs9880@gmail.com>
2023-12-25 00:58:00 +0900

    [UPDATE] Modify .env.local and fetchGraphQL.ts

--- relay/fetchGraphQL.ts
+++ relay/fetchGraphQL.ts
@@ -1,10 +1,10 @@
+import { BACKEND_BASE_URL } from "./../src/utils/constants";
 import fetch from "isomorphic-fetch";
 import type { Variables } from "relay-runtime";
 import { NEXT_PUBLIC_BACKEND_BASE_URL } from "../src/utils/constants";
 
 const fetchGraphQL = async (query: string, variables: Variables) => {
   console.log("isDev?", process.env.NODE_ENV);
-  console.log("fetch & url", NEXT_PUBLIC_BACKEND_BASE_URL);
   try {
     const response = await fetch(`${NEXT_PUBLIC_BACKEND_BASE_URL}/graphql`, {
       method: "POST",
