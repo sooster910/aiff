cc30ef6ff3ac57163f740759c473e803fbc5dca3
Hyunsu <hs9880@gmail.com>
2024-11-25 19:58:13 +0900

    fix:eb배포 중 peerdendency충돌로 next, react 버전 마이그레이션 - sentry/nextjs 가 16,17,18 요, next@12.3.4 는 react@"^17.0.2 || ^18.0.0-0" 요구로 인해 next 버전 기술 부채 해결을 위해 버전 업그레이드 결정 - next12,13 Image컴포넌트 속성 width, height이 required number로 변경 대응

--- src/components/ImageComp/index.tsx
+++ src/components/ImageComp/index.tsx
@@ -1,6 +1,9 @@
 import * as React from "react"
 // import { Image } from '@geist-ui/core'
-import Image from "next/image"
+
+// import Image from "next/image"
+import Image from "next/legacy/image"
+import {style} from "@motionone/dom";
 interface ImageCompProps {
   src: string
 }
@@ -9,9 +12,9 @@ const ImageComp: React.FunctionComponent<ImageCompProps> = ({src}) => {
   return (
     <Image
       src={src}
-      height="100%"
-      width="100%"
-      objectFit="cover"
+      width={100}
+      height={100}
+      objectFit={"cover"}
       style={{borderRadius: "10px", margin: "0 auto", flexShrink: 0}}
       alt={src}
     />
