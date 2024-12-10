8c26d0fa564ff06f7b942a9242e0ec7e2841deb9
Hyunsu <hs9880@gmail.com>
2024-12-10 22:12:24 +0900

    chore: 결제 승인 후 성공 페이지 처리 개선
    - 승인날짜를 보여줄 때 사용자의 브라우저의 타임존에 맞춰 로컬타임 으로 변경
    - 중복된 url인코딩을 URLSearchParams 만 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,71 +1,67 @@
-import "../styles/globals.css"
-import type {AppProps} from "next/app"
-import {RelayEnvironmentProvider} from "react-relay/hooks"
-import {GeistProvider, CssBaseline} from "@geist-ui/core"
-import {SWRConfig} from "swr"
-import {aiffAPI} from "@app/utils/aiffAPI"
-import {AuthProvider} from "@app/context/AuthProvider"
-import Navbar from "@app/components/Navbar"
-import MobileLayout from "@app/layouts/MobileLayout"
-import {useEnvironment} from "@app/../relay/environment"
-import Footer from "@app/components/Footer"
-import Script from "next/script"
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { RelayEnvironmentProvider } from "react-relay/hooks";
+import { GeistProvider, CssBaseline } from "@geist-ui/core";
+import { SWRConfig } from "swr";
+import { aiffAPI } from "@app/utils/aiffAPI";
+import { AuthProvider } from "@app/context/AuthProvider";
+import Navbar from "@app/components/Navbar";
+import MobileLayout from "@app/layouts/MobileLayout";
+import { useEnvironment } from "@app/../relay/environment";
+import Footer from "@app/components/Footer";
+import Script from "next/script";
 import mixpanel from "mixpanel-browser";
-import {useEffect} from "react";
-import {NEXT_PUBLIC_MIXPANEL_TOKEN} from "@app/utils/constants";
+import { useEffect } from "react";
+import { NEXT_PUBLIC_MIXPANEL_TOKEN } from "@app/utils/constants";
 
+function MyApp({ Component, pageProps }: AppProps) {
+  const environment = useEnvironment(pageProps);
 
-function MyApp({Component, pageProps}: AppProps) {
-  const environment = useEnvironment(pageProps)
-
-    useEffect(()=>{
-        mixpanel.init(NEXT_PUBLIC_MIXPANEL_TOKEN,  {
-            debug: true,
-            track_pageview: true,
-            persistence: "localStorage",
-        });
-    },[])
-
+  useEffect(() => {
+    mixpanel.init(NEXT_PUBLIC_MIXPANEL_TOKEN, {
+      debug: true,
+      track_pageview: true,
+      persistence: "localStorage",
+    });
+  }, []);
 
   return (
-      <>
-          <Script
-              strategy="afterInteractive"
-              src={"https://www.googletagmanager.com/gtag/js?id=G-X4P14LC53Q"}
-          />
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={"https://www.googletagmanager.com/gtag/js?id=G-X4P14LC53Q"}
+      />
 
-
-          <Script
-              dangerouslySetInnerHTML={{
-                  __html: `window.dataLayer = window.dataLayer || [];
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
                   function gtag(){dataLayer.push(arguments);}
                   gtag('js', new Date());
                   gtag('config', 'G-X4P14LC53Q');`,
+        }}
+      />
+      <RelayEnvironmentProvider environment={environment}>
+        <GeistProvider>
+          <CssBaseline>
+            <SWRConfig
+              value={{
+                fetcher: (resource) =>
+                  aiffAPI.get(resource).then((res) => res.data),
               }}
-          />
-              <RelayEnvironmentProvider environment={environment}>
-                  <GeistProvider>
-                      <CssBaseline>
-                          <SWRConfig
-                              value={{
-                                  fetcher: (resource) =>
-                                      aiffAPI.get(resource).then((res) => res.data),
-                              }}
-                          >
-                              <AuthProvider>
-                                  <MobileLayout maxWidth={"28rem"}>
-                                      <Navbar/>
-                                      <Component {...pageProps} />
-                                      <Footer/>
-                                  </MobileLayout>
-                              </AuthProvider>
-                          </SWRConfig>
-                      </CssBaseline>
-                  </GeistProvider>
-              </RelayEnvironmentProvider>
-      </>
-
-  )
+            >
+              <AuthProvider>
+                <MobileLayout maxWidth={"28rem"}>
+                  <Navbar />
+                  <Component {...pageProps} />
+                  <Footer />
+                </MobileLayout>
+              </AuthProvider>
+            </SWRConfig>
+          </CssBaseline>
+        </GeistProvider>
+      </RelayEnvironmentProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- src/pages/api/timeSlots/index.ts
+++ src/pages/api/timeSlots/index.ts
@@ -1,58 +1,56 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import fs from 'fs'
+import type { NextApiRequest, NextApiResponse } from "next";
+import fs from "fs";
 
-import { DateTime } from 'luxon';
-import { TimeSlotDTO } from '../../../types/timeslot';
+import { DateTime } from "luxon";
+import { TimeSlotDTO } from "../../../types/timeslot";
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
+  req: NextApiRequest,
+  res: NextApiResponse
 ) {
-    let response;
-    try {
-        
-   
-        const { startDate, store, regularClassId } = req.query;
-        
-        if (!startDate) {
-        return res.status(400).json({error:req})
+    console.log("handler 호출")
+  let response;
+  try {
+    const { startDate, store, regularClassId } = req.query;
+
+    if (!startDate) {
+      return res.status(400).json({ error: req });
     }
     if (Object.keys(req.query).length === 0) {
-        const timeslots = JSON.parse( fs.readFileSync('db/timeSlots.json', 'utf-8'));
-        
-        return res.status(200).json(timeslots)
-    } 
-    const timeslots = JSON.parse( fs.readFileSync('db/timeSlots.json', 'utf-8'));
-    // console.log("timeslots",timeslots)
-        const qsYear = DateTime.fromSQL(startDate as string).get('year')
-        const qsMonth = DateTime.fromSQL(startDate as string).get('month')
-        const qsDay = DateTime.fromSQL(startDate as string).get('day')
+      const timeslots = JSON.parse(
+        fs.readFileSync("db/timeSlots.json", "utf-8")
+      );
 
-        // console.log('year',year)
-        
-        const filteredByStartDate = timeslots.filter((timeslot:TimeSlotDTO) => {
-            const year = DateTime.fromSQL(timeslot?.startDate).get('year')
-            const month = DateTime.fromSQL(timeslot?.startDate).get('month')
-            const day = DateTime.fromSQL(timeslot?.startDate).get('day')
-
-            if (qsYear === year && qsMonth === month && qsDay === day) {
-                return timeslot
-            }
-        })
-        const filteredByStoreId =filteredByStartDate.filter((timeslot) => {
-            return Number(store) === Number(timeslot.storeId)
-        })
-        
-        response = filteredByStoreId.filter((timeslot)=> Number(timeslot.regularClassId)===Number(regularClassId))
-    } catch (err) {
-        console.log("err",err)
+      return res.status(200).json(timeslots);
     }
-        res.status(200).json(response)
-    
+    const timeslots = JSON.parse(fs.readFileSync("db/timeSlots.json", "utf-8"));
+    const qsYear = DateTime.fromSQL(startDate as string).get("year");
+    const qsMonth = DateTime.fromSQL(startDate as string).get("month");
+    const qsDay = DateTime.fromSQL(startDate as string).get("day");
+
+    const filteredByStartDate = timeslots.filter((timeslot: TimeSlotDTO) => {
+      const year = DateTime.fromSQL(timeslot?.startDate).get("year");
+      const month = DateTime.fromSQL(timeslot?.startDate).get("month");
+      const day = DateTime.fromSQL(timeslot?.startDate).get("day");
+
+      if (qsYear === year && qsMonth === month && qsDay === day) {
+        return timeslot;
+      }
+    });
+    const filteredByStoreId = filteredByStartDate.filter((timeslot) => {
+      return Number(store) === Number(timeslot.storeId);
+    });
+
+    response = filteredByStoreId.filter(
+      (timeslot) => Number(timeslot.regularClassId) === Number(regularClassId)
+    );
+  } catch (err) {
+    console.log("err", err);
+  }
+  res.status(200).json(response);
 }
 
 export function msFromUtc(date: string) {
-    
-    return new Date(date).getTime()
+  return new Date(date).getTime();
 }
