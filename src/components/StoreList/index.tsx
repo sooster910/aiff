c89ac5e11bc2ced38647596b07786168379bbc8a
Hyunsu <hs9880@gmail.com>
2024-12-22 03:52:58 +0900

    feat : 지점 리스트 fragment적용

--- src/components/StoreList/index.tsx
+++ src/components/StoreList/index.tsx
@@ -1,23 +1,38 @@
-import React from 'react';
-import {graphql, useFragment} from "react-relay";
-import {StoreList_query$key} from "../../../__generated__/StoreList_query.graphql";
+import { graphql, useFragment } from 'react-relay'
+import { StoreList_query$key } from '../../../__generated__/StoreList_query.graphql'
+import { Button, Select, SelectItem } from '@nextui-org/react'
+import { config } from '@app/config'
+import { FormikProps, FormikValues } from 'formik'
+import { FormValues } from '@app/components/SuspenseBooking'
 
 
 export const StoreListFragment = graphql`
     fragment StoreList_query on Query {
-        stores{ 
-            id
+        stores{
+            _id
             name
         }
     }
-`;
+`
 
-function StoreList({queryRef}:{queryRef:StoreList_query$key}) {
-    const storeList = useFragment(StoreListFragment, queryRef);
-    return (
-    <><h1>StoreList</h1></>
-
-    );
+type StoreListProps = {
+  queryRef: StoreList_query$key,
+  value: FormikProps<FormValues>['values']['store']
+  onChange: (id) => void
 }
 
-export default StoreList;
+function StoreList({ queryRef, value, onChange }: StoreListProps) {
+  const storeList = useFragment(StoreListFragment, queryRef)
+  const availableStores = storeList.stores.filter((store) => config.CURRENT_AVAILABLE_STORE.includes(store.name))
+
+  return (
+    <Select className="max-w-xs" selectedKeys={value} onSelectionChange={onChange}
+            variant={'underlined'}>
+      {availableStores.map((store) => (
+        <SelectItem key={store._id}>{store.name}</SelectItem>
+      ))}
+    </Select>
+  )
+}
+
+export default StoreList
