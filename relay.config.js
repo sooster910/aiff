69a44e76c4a68b54f705bbde16c4c852dd6514a4
Hyunsu <hs9880@gmail.com>
2024-12-22 03:51:26 +0900

    chore: relay directive 추가 설정 schema 파일 적용

--- relay.config.js
+++ relay.config.js
@@ -1,12 +1,13 @@
 module.exports = {
-  src: "./",
-  artifactDirectory: "__generated__",
-  schema: "schema.graphql",
+  src: './',
+  artifactDirectory: '__generated__',
+  schema: 'schema.graphql',               // 기존 백엔드 schema
+  schemaExtensions: ['./schema.extensions.graphql'],
   exclude: [
-    "**/node_modules/**",
-    "**/.next/**",
-    "**/__mocks__/**",
-    "**/__generated__/**",
+    '**/node_modules/**',
+    '**/.next/**',
+    '**/__mocks__/**',
+    '**/__generated__/**',
   ],
-  language: "typescript",
-};
+  language: 'typescript',
+}
