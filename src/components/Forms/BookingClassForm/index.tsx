d27b6e6ac53f1e583d288462480426db4f5c065d
Hyunsu <hs9880@gmail.com>
2024-12-22 03:47:38 +0900

    feat: BookingClassForm 컴포넌트 추가 및 Store 데이터 로딩을 위한 Suspense 사용
    - useFormik를 사용하여 폼 관리 기능 구현
    - 날짜 및 스토어 선택 핸들러 추가하여 폼 값 업데이트
    - 선택된 스토어와 날짜에 따라 Store 컴포넌트를 lazy-load 하기 위해 Suspense 사용
    - preloaded query 데이터를 StoreList와 Store 컴포넌트에 전달
    - storeId 업데이트를 위한 useRouter 활용하여 네비게이션 및 상태 관리 구현

--- src/components/Forms/BookingClassForm/index.tsx
+++ src/components/Forms/BookingClassForm/index.tsx
@@ -1,22 +1,68 @@
-import React from 'react';
-import { PreloadedQuery, usePreloadedQuery} from "react-relay";
-import StoreList from "@app/components/StoreList";
-import {BookingQuery} from "@app/pages/booking";
-import {bookingQuery as BookingQueryType} from "../../../../__generated__/bookingQuery.graphql";
+import React, { startTransition, Suspense, useState, useTransition } from 'react'
+import { graphql, PreloadedQuery, usePreloadedQuery, useQueryLoader } from 'react-relay'
+import StoreList, { StoreListFragment } from '@app/components/StoreList'
+import { BookingQuery } from '@app/pages/booking'
+import { bookingQuery as BookingQueryType } from '../../../../__generated__/bookingQuery.graphql'
+import { useFormik } from 'formik'
+
+import { SelectDate } from '@app/components/SelectDate'
+import { DateTime } from 'luxon'
+import { useRouter } from 'next/router'
+import { Store } from '@app/components/Store'
 
 type BookingClassFormProps = {
-    queryRef: PreloadedQuery<BookingQueryType>
+  queryRef: PreloadedQuery<BookingQueryType>
 }
 
-function BookingClassForm({queryRef}:BookingClassFormProps) {
-    const data = usePreloadedQuery(BookingQuery, queryRef);
 
-    return (
-        <>
-            <h1>BookingClassForm</h1>
-            <StoreList queryRef={data}/>
-        </>
-    );
+function BookingClassForm({ queryRef }: BookingClassFormProps) {
+  const router = useRouter()
+  const data = usePreloadedQuery(BookingQuery, queryRef)
+
+  console.log('data', data)
+  const formik = useFormik({
+    initialValues: {
+      date: new Date(),
+      store: '1',
+    },
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2))
+    },
+  })
+
+
+  const handleOnDayClick = (day) => {
+
+    startTransition(() => {
+      formik.setFieldValue('date', day, true)
+    })
+    const date = DateTime.fromJSDate(day).setZone('Asia/Seoul').toISODate()
+
+    console.log('선택날짜', date)
+
+  }
+
+  const handleOnStoreClick = (storeId) => {
+    formik.setFieldValue('store', storeId, true)
+    router.replace({
+      pathname: '/booking',
+      query: { ...router.query, storeId },
+    })
+  }
+  return (
+
+    <form onSubmit={formik.handleSubmit}
+          className={'flex flex-col items-center justify-center w-full'}>
+      <StoreList queryRef={data} onChange={handleOnStoreClick} value={formik.values.store} />
+      <SelectDate onChange={handleOnDayClick}
+                  value={formik.values.date} />
+      <Suspense fallback={<p>..classLoading</p>}>
+        <Store storeId={formik.values.store} selectedDate={formik.values.date} />
+      </Suspense>
+    </form>
+
+  )
 }
 
-export default BookingClassForm;
+export default BookingClassForm
+
