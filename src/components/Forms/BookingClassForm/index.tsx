d77e3f6be5ba6288f34d1624aa95cab3c926e66c
Hyunsu <hs9880@gmail.com>
2024-12-25 13:02:43 +0900

    chore : seLazyLoadQuery 적용 및 Form 상위 구조 수정
    - Form 컴포넌트에 useLazyLoadQuery를 적용하여 데이터 로드 방식 개선
    - 상위 구조를 리팩터링하여 컴포넌트 재사용성과 가독성 향상

--- src/components/Forms/BookingClassForm/index.tsx
+++ src/components/Forms/BookingClassForm/index.tsx
@@ -1,80 +1,68 @@
-import React, { startTransition, Suspense, useState, useTransition } from 'react'
-import { graphql, PreloadedQuery, usePreloadedQuery, useQueryLoader } from 'react-relay'
-import StoreList, { StoreListFragment } from '@app/components/StoreList'
-import { BookingQuery } from '@app/pages/booking'
-import { bookingQuery as BookingQueryType } from '../../../../__generated__/bookingQuery.graphql'
-import { useFormik } from 'formik'
-
+import { RegularClasses } from '@app/components/RegularClasses'
 import { SelectDate } from '@app/components/SelectDate'
+import { useFormik } from 'formik'
 import { DateTime } from 'luxon'
-import { useRouter } from 'next/router'
+import { useState } from 'react'
+import { graphql, PreloadedQuery, useLazyLoadQuery } from 'react-relay'
+import type { BookingClassFormQuery as BookingQueryType } from '../../../../__generated__/BookingClassFormQuery.graphql'
 
 type BookingClassFormProps = {
   queryRef: PreloadedQuery<BookingQueryType>
 }
 
-export const BookingClassFormFragment = graphql`
-    fragment BookingClassForm_query on Query
-    @argumentDefinitions(
-        after: {type: "String"}
-        first: {type: "Int!"}
-        date: {type: "Date!"}
-        storeId:{type:"String!"}
-    ) {
-
-        stores(where: {_id: $storeId}) {
-            _id
-            id
-            name
-            description
-            ...RegularClassesFragment @arguments(after:$after, first:$first, date:$date)
-        }
+export const BookingClassFormQuery = graphql`
+  query BookingClassFormQuery($storeId: String!, $date: Date!) {
+    stores(where: { _id: $storeId }) {
+      _id
+      id
+      description
+      ...RegularClassesFragment @arguments(date: $date, after: null, first: 1)
     }
+  }
 `
+export type FormValues = {
+  date: Date
+  store: string //TODO literal type으로 변경
+}
 
-function BookingClassForm({ queryRef }: BookingClassFormProps) {
-  const router = useRouter()
-  const data = usePreloadedQuery(BookingQuery, queryRef)
-
-  console.log('BookingClassForm', data)
-  const formik = useFormik({
+export const BookingClassForm = () => {
+  const [queryVariables, setQueryVariables] = useState({
+    storeId: '1',
+    date: DateTime.now().setZone('Asia/Seoul').toISODate(),
+  })
+  const data = useLazyLoadQuery<BookingQueryType>(
+    BookingClassFormQuery,
+    queryVariables
+  )
+  const formik = useFormik<FormValues>({
     initialValues: {
       date: new Date(),
       store: '1',
     },
-    onSubmit: values => {
+    onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2))
     },
   })
 
-
-  const handleOnDayClick = (day) => {
-
-    formik.setFieldValue('date', day, true)
-    const date = DateTime.fromJSDate(day).setZone('Asia/Seoul').toISODate()
-
-    console.log('선택날짜', date)
-
-  }
-
   const handleOnStoreClick = (storeId) => {
     formik.setFieldValue('store', storeId, true)
   }
+  function handleOnDayClick(selected: Date) {
+    setQueryVariables((prev) => ({
+      ...prev,
+      date: DateTime.fromJSDate(selected).setZone('Asia/Seoul').toISODate(),
+    }))
+    formik.setFieldValue('date', selected, true)
+  }
   return (
-
-    <form onSubmit={formik.handleSubmit}
-          className={'flex flex-col items-center justify-center w-full'}>
-      {/*<StoreList queryRef={data} onChange={handleOnStoreClick} value={formik.values.store} />*/}
-      <SelectDate onChange={handleOnDayClick}
-                  value={formik.values.date} />
-      <Suspense fallback={<p>..classLoading</p>}>
-        {/*<StoreDetail store={data.stores[0]} />*/}
-        {/*<RegularClasses store={data.stores[0]} />*/}
-      </Suspense>
+    <form
+      onSubmit={formik.handleSubmit}
+      className={'flex flex-col items-center justify-center w-full'}
+    >
+      {/* <StoreList queryRef={data} onChange={handleOnStoreClick} value={formik.values.store} /> */}
+      <SelectDate onChange={handleOnDayClick} value={formik.values.date} />
+      <RegularClasses regularClasses={data.stores[0]} />
+      {/*<StoreDetail store={data.stores[0]} />*/}
     </form>
-
   )
 }
-
-export default BookingClassForm
-
--- src/pages/booking/index.tsx
+++ src/pages/booking/index.tsx
@@ -1,41 +1,11 @@
-import * as React from 'react'
-import { useEffect } from 'react'
-import { graphql, useQueryLoader } from 'react-relay'
-import BookingClassForm from '@app/components/Forms/BookingClassForm'
+import { BookingClassForm } from '@app/components/Forms/BookingClassForm'
 import SuspenseWrapper from '@app/components/SuspenseWrapper'
-import { bookingQuery as BookingQueryType } from '../../../__generated__/bookingQuery.graphql'
-import { DateTime } from 'luxon'
-
-export const BookingQuery = graphql`
-    query bookingQuery( $after: String, $first: Int!, $date: Date!, $storeId: String!) {
-      
-        ...BookingClassForm_query @arguments(after: $after, first: $first, date: $date, storeId:$storeId)
-    }
-`
-
 
 const BookingPage = () => {
-  const [queryRef, loadQuery] = useQueryLoader<BookingQueryType>(BookingQuery)
-  const initialValues = {
-    storeId: '1',
-    date: DateTime.now().setZone('Asia/Seoul').toISODate(),
-    after: null,
-    first: 1,
-  }
-
-  if (queryRef == null) {
-    loadQuery(initialValues)
-    return <p>...loading</p>
-  }
-
   return (
-    <>
-      {/* A component that uses Suspense-based */}
-
-      <SuspenseWrapper fallback={<p>...loading</p>}>
-        {queryRef && <BookingClassForm queryRef={queryRef} />}
-      </SuspenseWrapper>
-    </>
+    <SuspenseWrapper fallback={<p>...loading</p>}>
+      <BookingClassForm />
+    </SuspenseWrapper>
   )
 }
 export default BookingPage
