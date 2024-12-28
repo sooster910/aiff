45e31506d937cf8fb25f6df91ab33117196fe79f
Hyunsu <hs9880@gmail.com>
2024-12-28 11:41:11 +0900

    chore: 예약 절차 개선을 위한 바텀 시트 이벤트 핸들러 및 프리로드 적용
    - 예약 절차 간소화를 위한 UI/UX 최적화
    - relay 프리로드 적용

--- src/components/TimeSlot/index.tsx
+++ src/components/TimeSlot/index.tsx
@@ -1,65 +1,43 @@
+import { Button } from '@nextui-org/react'
 import { DateTime } from 'luxon'
-import * as React from 'react'
 import { graphql, useFragment } from 'react-relay'
 import { TimeSlotFragment$key } from '../../../__generated__/TimeSlotFragment.graphql'
 
 interface TimeSlotProps {
-  timeSlot: TimeSlotFragment$key;
+  timeSlot: TimeSlotFragment$key
+  onClick: (timeSlotId: string) => void
 }
 
 export const TimeSlotFragment = graphql`
-    fragment TimeSlotFragment on TimeSlot {
-        _id
-        id
-        name
-        startDateTime
-        endDateTime
-        regularClassId
-        price
-        currentBookingCount
-        maximumBookingCount
-        regularClass {
-            _id
-            description
-            duration
-            name
-            price
-        }
+  fragment TimeSlotFragment on TimeSlot {
+    _id
+    id
+    name
+    startDateTime
+    endDateTime
+    regularClassId
+    price
+    currentBookingCount
+    maximumBookingCount
+    regularClass {
+      _id
+      description
+      duration
+      name
+      price
     }
+  }
 `
-export const TimeSlot = ({ timeSlot }: TimeSlotProps) => {
+export const TimeSlot = ({ timeSlot, onClick }: TimeSlotProps) => {
   const data = useFragment(TimeSlotFragment, timeSlot)
   return (
-    <div
-      className={'flex flex-nowrap items-center py-4'}
-    >
-      <div
-        className={'text-sm mr-2 px-3 py-2 text-amber-50 bg-red-300 rounded cursor-pointer'}
+    <div className={'flex flex-nowrap items-center py-4'}>
+      <Button
+        color="primary"
+        variant={'ghost'}
         key={data.id}
-
-        onClick={async () => {
-          //   handleRegularClassClicked(singleClass?.id);
-          //   await setFieldValue("timeSlot", timeslot?.id);
-          //   await setFieldValue(
-          //     "maximumBookingCount",
-          //     Number(timeslot?.maximumBookingCount)
-          //   );
-          //   await setFieldValue(
-          //     "currentBookingCount",
-          //     Number(timeslot?.currentBookingCount)
-          //   );
-          //   await setFieldValue("startDateTime", timeslot?.startDateTime);
-          //   setFieldValue("classPrice", singleClass?.price);
-          //   if (Number(values?.qty) > 0) {
-          //     setFieldValue(
-          //       "totalAmount",
-          //       Number(values?.qty) * Number(values?.classPrice)
-          //     );
-          //   }
-        }}
-      >
-        {`${DateTime.fromISO(data.startDateTime).toFormat('HH:mm')}`}
-      </div>
+        onPress={() => onClick(data._id)}
+      >{`${DateTime.fromISO(data.startDateTime).toFormat('HH:mm')}`}</Button>
     </div>
   )
 }
--- src/components/TimeSlots/index.tsx
+++ src/components/TimeSlots/index.tsx
@@ -1,13 +1,52 @@
-import { TimeSlotFragment$key } from '../../../__generated__/TimeSlotFragment.graphql'
 import { TimeSlot } from '@app/components/TimeSlot'
-
-
+import { useState } from 'react'
+import { graphql, useQueryLoader } from 'react-relay'
+import { TimeSlotFragment$key } from '../../../__generated__/TimeSlotFragment.graphql'
+import { TimeSlotsReservationQuery as TimeSlotsReservationQueryType } from '../../../__generated__/TimeSlotsReservationQuery.graphql'
+import { ReservationBottomSheet } from '../ReservationModal'
 type TimeSlotsProps = {
   timeSlots: ReadonlyArray<TimeSlotFragment$key>
 }
+//TODO: fragment
+export const timeSlotReservationQuery = graphql`
+  query TimeSlotsReservationQuery($timeSlotId: String!) {
+    timeSlot(where: { _id: $timeSlotId, skip: false }) {
+      ...TimeSlotFragment
+    }
+  }
+`
 
 export const TimeSlots = ({ timeSlots }: TimeSlotsProps) => {
+  const [queryReference, loadQuery] =
+    useQueryLoader<TimeSlotsReservationQueryType>(timeSlotReservationQuery)
+  //TODO: Refactor hook for modal
+  const [modalOpen, setModalOpen] = useState(false)
+  const handleCloseModal = () => {
+    setModalOpen(false)
+  }
+
+  const handleTimeSlotClicked = (id: string) => {
+    console.log('click')
+    setModalOpen(true)
+    loadQuery({ timeSlotId: id })
+  }
+
   return (
-    <>{timeSlots.map((timeSlot, idx) => <TimeSlot key={idx} timeSlot={timeSlot} />)}</>
+    <>
+      {timeSlots.map((timeSlot, idx) => (
+        <TimeSlot
+          key={`timeslot-${idx}`}
+          timeSlot={timeSlot}
+          onClick={handleTimeSlotClicked}
+        />
+      ))}
+      {modalOpen && queryReference && (
+        <ReservationBottomSheet
+          isOpen={modalOpen}
+          onClose={handleCloseModal}
+          selectedQueryReference={queryReference}
+        />
+      )}
+    </>
   )
 }
