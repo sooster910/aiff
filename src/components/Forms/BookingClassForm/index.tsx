52b0b2f3eab1ebf17823180c991babb68099c5c2
Hyunsu <hs9880@gmail.com>
2024-12-28 11:17:58 +0900

    chore : formik 상태 관리 제거
    - 관리해야 할 상태가 date 하나로 의사결정
    - 이에 따라 form 관리 불필요

--- src/components/Forms/BookingClassForm/index.tsx
+++ src/components/Forms/BookingClassForm/index.tsx
@@ -1,15 +1,10 @@
 import { RegularClasses } from '@app/components/RegularClasses'
 import { SelectDate } from '@app/components/SelectDate'
-import SuspenseWrapper from '@app/components/SuspenseWrapper'
 import { useFormik } from 'formik'
 import { DateTime } from 'luxon'
 import { useState, useTransition } from 'react'
 import { graphql, PreloadedQuery, useLazyLoadQuery } from 'react-relay'
 import type { BookingClassFormQuery as BookingQueryType } from '../../../../__generated__/BookingClassFormQuery.graphql'
-
-type BookingClassFormProps = {
-  queryRef: PreloadedQuery<BookingQueryType>
-}
 
 export const BookingClassFormQuery = graphql`
   query BookingClassFormQuery($storeId: String!, $date: Date!) {
@@ -36,19 +31,6 @@ export const BookingClassForm = () => {
     BookingClassFormQuery,
     queryVariables
   )
-  const formik = useFormik<FormValues>({
-    initialValues: {
-      date: new Date(),
-      store: '1',
-    },
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2))
-    },
-  })
-
-  const handleOnStoreClick = (storeId) => {
-    formik.setFieldValue('store', storeId, true)
-  }
 
   function handleOnDayClick(selected: Date) {
     startTransition(() => {
@@ -57,23 +39,16 @@ export const BookingClassForm = () => {
         date: DateTime.fromJSDate(selected).setZone('Asia/Seoul').toISODate(),
       }))
     })
-    // formik.setFieldValue('date', selected, true)
   }
 
   return (
-    <form
-      onSubmit={formik.handleSubmit}
-      className={'relative flex-1 flex-col items-center justify-center w-full'}
-    >
+    <>
       {/* <StoreList queryRef={data} onChange={handleOnStoreClick} value={formik.values.store} /> */}
       <SelectDate
         onChange={handleOnDayClick}
         value={DateTime.fromISO(queryVariables.date).toJSDate()}
       />
-
       <RegularClasses regularClasses={data.stores[0]} />
-
-      {/*<StoreDetail store={data.stores[0]} />*/}
-    </form>
+    </>
   )
 }
