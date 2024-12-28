832c4d4d3e1aac967fc873dc5770fbdca8dfaa59
Hyunsu <hs9880@gmail.com>
2024-12-28 14:03:36 +0900

    chore : [WIP] Relay 프로바이더 타입 및 환경 코드 적용

--- src/pages/RelayProvider.tsx
+++ src/pages/RelayProvider.tsx
@@ -1,11 +1,11 @@
 import {RelayEnvironmentProvider} from "react-relay";
 
-interface RelayProviderProps {
-    environment: any; // 실제 환경에 맞는 타입으로 변경하세요
-    children: any;
-}
+// interface RelayProviderProps {
+//     environment: any; // 실제 환경에 맞는 타입으로 변경하세요
+//     children: any;
+// }
 
-const  RelayProvider=({ environment, children }: RelayProviderProps) =>{
+const  RelayProvider=({ environment, children }) =>{
     return (
         // @ts-ignore
         <RelayEnvironmentProvider environment={environment} >
