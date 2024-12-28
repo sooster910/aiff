5f65be09c7fa5914e84dfdf0345e976effe8d45f
Hyunsu <hs9880@gmail.com>
2024-12-28 12:36:31 +0900

    chore: 날짜 선택시 스타일 개선
    - 추후 변경될 가능성이 매우 큰 코드
    - tailwind용으로 react-day-picker에서 제시한 defualtClassNames 에 대한 이해 부족

--- src/components/SelectDate/index.tsx
+++ src/components/SelectDate/index.tsx
@@ -1,6 +1,10 @@
 import { FormValues } from '@app/components/Forms/BookingClassForm'
 import { FormikProps } from 'formik'
-import { DayPicker, OnSelectHandler } from 'react-day-picker'
+import {
+  DayPicker,
+  getDefaultClassNames,
+  OnSelectHandler,
+} from 'react-day-picker'
 import 'react-day-picker/style.css'
 
 type SelectDateProps = {
@@ -10,9 +14,12 @@ type SelectDateProps = {
 }
 
 export const SelectDate = ({ onChange, value, isPending }: SelectDateProps) => {
+  const defaultClassNames = getDefaultClassNames()
+
   return (
-    <div className="datetime-selector">
+    <div className="datetime-selector flex flex-col items-center ">
       <DayPicker
+        classNames={{ selected: 'bg-primary-300', chevron: 'text-gray-400' }}
         captionLayout="dropdown"
         mode="single"
         numberOfMonths={1}
