853659c3e44a720bde9525138d1d773ef448f91b
Hyunsu <hs9880@gmail.com>
2024-12-29 16:07:20 +0900

    chore: prebuild yarn

--- .platform/hooks/prebuild/01_yarn_install.sh
+++ .platform/hooks/prebuild/01_yarn_install.sh
@@ -1,6 +1,9 @@
 # .platform/hooks/prebuild/01_yarn_install.sh
 #!/bin/bash
-set -e
-curl -o- -L https://yarnpkg.com/install.sh | bash
-export PATH="$HOME/.yarn/bin:$PATH"
-yarn install
+#set -e
+#curl -o- -L https://yarnpkg.com/install.sh | bash
+#export PATH="$HOME/.yarn/bin:$PATH"
+#yarn install
+#!/bin/bash
+cd /var/app/staging
+yarn install --frozen-lockfile
