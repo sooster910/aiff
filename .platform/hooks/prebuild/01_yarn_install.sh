7a4537196b88f0f482419b37d639a9dc96ba364a
Hyunsu <hs9880@gmail.com>
2024-12-29 21:40:25 +0900

    fis: 포맷 에러

--- .platform/hooks/prebuild/01_yarn_install.sh
+++ .platform/hooks/prebuild/01_yarn_install.sh
@@ -1,9 +1,19 @@
-# .platform/hooks/prebuild/01_yarn_install.sh
 #!/bin/bash
-#set -e
-#curl -o- -L https://yarnpkg.com/install.sh | bash
-#export PATH="$HOME/.yarn/bin:$PATH"
-#yarn install
-#!/bin/bash
+set -e
+echo "Starting Yarn installation in staging directory"
+
+# 디렉토리 확인
+if [ -d "/var/app/staging" ]; then
+  echo "Staging directory exists"
+else
+  echo "Staging directory does not exist"
+  exit 1
+fi
+
+# 디렉토리 이동
 cd /var/app/staging
+echo "Current directory: $(pwd)"
+
+# Yarn 패키지 설치
 yarn install --frozen-lockfile --production
+echo "Yarn installation completed"
