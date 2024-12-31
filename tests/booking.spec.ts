1c631a2c7863805bb6b25ee78016716ba6344d09
Hyunsu <hs9880@gmail.com>
2024-12-31 09:17:58 +0900

    test: 메인 페이지 test 작성

---
+++ tests/booking.spec.ts
@@ -0,0 +1,19 @@
+import { expect, test } from '@playwright/test'
+
+test.describe('AIFF 예약 시스템', () => {
+  test('메인 페이지에 지점,시간표 예약링크가 노출되어야 한다.', async ({
+    page,
+  }) => {
+    await page.goto('/')
+    const bookingLink = page.getByRole('link', { name: '지점 시간표 / 예약' })
+    console.log('bookingLinkk', bookingLink)
+    // 링크가 존재하는지 확인
+    expect(bookingLink).toBeVisible()
+
+    // 링크의 href 속성이 올바른지 확인
+    expect(bookingLink).toHaveAttribute('href', '/booking')
+
+    // 링크 텍스트가 올바른지 확인
+    expect(bookingLink).toHaveText('지점 시간표 / 예약')
+  })
+})
