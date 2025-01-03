03aa789c6a01e8352250b1580db051754b7b8a70
Hyunsu <hs9880@gmail.com>
2025-01-03 13:25:12 +0900

    chore: [WIP] esm모듈 변경

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ import tailwindScrollbar from 'tailwind-scrollbar'
 /** @type {import('tailwindcss').Config} */
 import { nextui } from '@nextui-org/react'
 
-module.exports = {
+export default {
   content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
