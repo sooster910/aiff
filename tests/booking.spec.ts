44a8981592992da1b1b3fe095aaa3b246162b628
Hyunsu <hs9880@gmail.com>
2025-01-03 13:35:21 +0900

    chore: 404page추가 및 test await키워드 추가

--- tests/booking.spec.ts
+++ tests/booking.spec.ts
@@ -1,5 +1,4 @@
 import { expect, test } from '@playwright/test'
-import { response } from 'express'
 
 test.describe('AIFF 예약 시스템', () => {
   test('메인 페이지에 지점,시간표 예약링크가 노출되어야 한다.', async ({
@@ -7,14 +6,16 @@ test.describe('AIFF 예약 시스템', () => {
   }) => {
     await page.goto('/')
     const bookingLink = page.getByRole('link', { name: '지점 시간표 / 예약' })
+    await page.screenshot({ path: 'screenshot.png' })
+
     console.log('bookingLinkk', bookingLink)
     // 링크가 존재하는지 확인
-    expect(bookingLink).toBeVisible()
+    await expect(bookingLink).toBeVisible()
 
     // 링크의 href 속성이 올바른지 확인
-    expect(bookingLink).toHaveAttribute('href', '/booking')
+    await expect(bookingLink).toHaveAttribute('href', '/booking')
 
     // 링크 텍스트가 올바른지 확인
-    expect(bookingLink).toHaveText('지점 시간표 / 예약')
+    await expect(bookingLink).toHaveText('지점 시간표 / 예약')
   })
 })
